1df53c5933d7e7a65f0addde3159b1a51a28537f
MYR <myr@pop-os.localdomain>
2020-04-10 10:49:51 +0200

    tmp

--- build.sh
+++ build.sh
@@ -11,26 +11,26 @@ function flamingo(){
 
   if [ $# = 0 ]
     then
-      ARG="-r"
+      ARG=\"-r\"
     else
-      ARG="$1"
+      ARG=\"$1\"
   fi
 
-  if [ "$ARG" = "-u" ]
+  if [ \"$ARG\" = \"-u\" ]
     then
-      docker run --rm -ti --init --runtime=nvidia --ipc=host --volume="$PWD:/app" -e NVIDIA_VISIBLE_DEVICES=all -u 1000:1000 -w /app pytorch:flamingo bash
+      docker run --rm -ti --init --runtime=nvidia --ipc=host --volume=\"$PWD:/app\" -e NVIDIA_VISIBLE_DEVICES=all -u 1000:1000 -w /app "$1" bash
   fi
-  if [ "$ARG" = "-p" ]
+  if [ \"$ARG\" = \"-p\" ]
     then
-      docker run --rm -ti --runtime=nvidia --init --ipc=host --volume="$PWD:/app" -e NVIDIA_VISIBLE_DEVICES=all -w /app pytorch:flamingo python "$2"
+      docker run --rm -ti --runtime=nvidia --init --ipc=host --volume=\"$PWD:/app\" -e NVIDIA_VISIBLE_DEVICES=all -w /app "$1" python \"$2\"
   fi
-  if [ "$ARG" = "-d" ]
+  if [ \"$ARG\" = \"-d\" ]
     then
-      docker run -d   -ti --init --runtime=nvidia --ipc=host --volume="$PWD:/app" -e NVIDIA_VISIBLE_DEVICES=all -w /app pytorch:flamingo bash
+      docker run -d -ti --init --runtime=nvidia --ipc=host --volume=\"$PWD:/app\" -e NVIDIA_VISIBLE_DEVICES=all -w /app "$1" bash
   fi
-  if [ "$ARG" = "-r" ]
+  if [ \"$ARG\" = \"-r\" ]
     then
-      docker run --rm -ti --init --runtime=nvidia --ipc=host --volume="$PWD:/app" -e NVIDIA_VISIBLE_DEVICES=all -w /app pytorch:flamingo bash
+      docker run --rm -ti --init --runtime=nvidia --ipc=host --volume=\"$PWD:/app\" -e NVIDIA_VISIBLE_DEVICES=all -w /app "$1" bash
   fi
 }
 
